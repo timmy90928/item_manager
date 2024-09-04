4f5e18797c4260c850a2da32a39187fb52d07299
Wei-Wen Wu <timmy90928@gmail.com>
2024-09-04 16:14:23 +0800

    feat: Initial

---
+++ utils/db.py
@@ -0,0 +1,97 @@
+from sqlite3 import connect,Cursor
+from utils.utils import msgw
+
+class database:
+    """
+    https://www.1keydata.com/tw/sql/sqlinsert.html
+
+    2023-08-12
+    """
+    def __init__(self,filename:str) -> None:
+        assert '.db' in filename,'[FileError] 資料庫應為 .db 檔.'
+        self.conn = connect(filename,check_same_thread=False)
+        self.cursor:Cursor = self.conn.cursor()
+
+    def __call__(self,exe:str) -> list:
+        return self.cursor.execute(exe).fetchall()
+    
+    def commit(self) -> None:
+        self.conn.commit()
+
+    def close(self) -> None:
+        self.conn.close()
+
+    def get_col(self,table,col_name,search:list=[]):
+        """
+        ```
+        print(db.get_col('Hospital','name',['name','%小%']))
+        print(db.get_col('Hospital','name,area',['name','%小%']))
+        ```
+        """
+        # SELECT DISTINCT  無重複
+        if search==[]:
+            return self.__call__(f"SELECT {col_name} FROM {table}")
+        else:
+            return self.__call__(f"SELECT {col_name} FROM {table} WHERE {search[0]} LIKE '{search[1]}'")
+        
+    def get_row(self,table:str,row_name:list,col_name=None):
+        """
+        ```
+        print(db.get_row('Data',['ID','1'],"hospital_name,rdate,ddate"))
+        ```
+        """
+        col_name='*' if col_name==None else f"{col_name}"
+        return self.__call__(f"SELECT {col_name} FROM {table} WHERE {row_name[0]} = '{row_name[1]}'")
+        
+    def delete(self,table:str,row_name:list):
+        """
+        db.delete('Data',['ID','1'])
+        """
+        if msgw('刪除','請問是否要刪除?',1,0)==1:
+            self.__call__(f"DELETE FROM {table} WHERE {row_name[0]} = '{row_name[1]}'")
+            self.commit()
+            return True
+        else:
+            return False
+        
+    def add(self,table:str,col_name:str,value:str,commit:bool=True):
+        """
+        ```
+        db.add('Hospital','name,area',"'2','2'")
+        ```
+        """
+        self.__call__(f"INSERT INTO {table} ({col_name}) VALUES ({value})")
+        if commit==True: self.commit()
+
+    def revise(self,table:str,col_name:list,revise_col:list):
+        """
+        ```
+        db.revise('Data',['ID','2'],['hospital_name','2'])
+        ```
+        """
+        self.__call__(f"UPDATE  {table} SET {revise_col[0]} = '{revise_col[1]}' WHERE {col_name[0]} = '{col_name[1]}'")
+        self.commit()
+
+if __name__ == "__main__":
+    db=database(r'C:\ProgramData\WELLS\project.db')
+
+    # hospital_name=YAML(r'C:\Users\jetbo\Downloads\project\project.yaml')['hospital_name']
+    # for _1,_2 in hospital_name.items():
+    #     try:
+    #         db(f"INSERT INTO Hospital (name,area) VALUES ('{_1}', '{_2}')")
+    #     except:
+    #         print(_1,_2 )
+    # db.commit()
+
+    # db.add('Data','hospital_name,rdate,ddate,patient,doctor,item,option,note,amount,create_time',f"'1','1','1','1','1','1','1','1','1','{datetime.now()}'")
+    # a=db("SELECT amount*discount FROM Data WHERE ID = '1'")
+    # db.revise('Data',['ID','1'],['real_amount',db("SELECT amount*discount FROM Data WHERE ID = '1'")[0][0]])
+
+    
+    
+
+
+
+
+
+
---
+++ utils/utils.py
@@ -0,0 +1,41 @@
+from datetime import timedelta,datetime
+
+def read_card() -> str:
+    """
+    """
+    print("請刷卡...")
+    card_data = input()  # Wait for user to swipe card from card reader.
+    return card_data
+    print(f"讀取到的卡資料：{card_data}")
+
+def msgw(title:str="Title", text:str="contant", style:int=0, time:int=0) -> int:
+    """
+    ctypes.windll.user32.MessageBoxTimeoutW()
+
+    Styles
+    ------
+    ```
+    0 : OK
+    1 : OK | Cancel
+    2 : Abort | Retry | Ignore
+    3 : Yes | No | Cancel
+    4 : Yes | No
+    5 : Retry | No 
+    6 : Cancel | Try Again | Continue
+    ```
+
+    Example
+    -------
+    ```
+    msg=msgw('title','contant',0,1000)  # time (ms)
+    print(msg)
+    ```
+    """
+    import ctypes
+    # MessageBoxTimeoutW(父窗口句柄,消息內容,標題,按鈕,語言ID,等待時間)
+    return ctypes.windll.user32.MessageBoxTimeoutW(0, text, title, style,0,time)
+
+def now_time() -> str:
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
