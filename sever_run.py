29328d9e76bde773be634e028bcedc23a3950346
Wei-Wen Wu <timmy90928@gmail.com>
2024-09-05 15:02:28 +0800

    feat(url): show

--- sever_run.py
+++ sever_run.py
@@ -8,5 +8,9 @@ app = Flask("Key Manager")
 def index():
     return render_template('index.html')
 
+@app.route("/show/<table_name>")
+def show(table_name):
+    return render_template('show.html',name=table_name,datas=db.get_col(table_name,'*',['id','%']),heads=db.get_head(table_name))
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port="120")
