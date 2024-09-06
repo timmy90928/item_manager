a05b897aa1ca8b829e9de336e5b082f50d80eb99
ChrisEren <zeroandbeta2211@gmail.com>
2024-09-06 22:57:14 +0800

    feat: item list page design

--- server_run.py
+++ server_run.py
@@ -19,7 +19,19 @@ def admin():
 
 @app.route("/itemlist")
 def itemlist():
-    return render_template('/admin/item_list.html')
+    table_name = 'item'
+    item = db.get_col(table_name, '*', ['id', '%'])
+    # 確保數據格式對應表格標題
+    formatted_items = []
+    for item in item:
+        formatted_items.append([
+            item[0],  # ID
+            item[1],  # 物品名稱
+            item[2],  # 財產編號
+            item[3],  # 借出狀況
+            item[4]   # 備註
+        ])
+    return render_template('/admin/item_list.html', items=formatted_items)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port="429")
